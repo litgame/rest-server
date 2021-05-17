aaeee16344c3d4dcd56b5680d7e6603f628a6f92
Alexey Volkov <asgalex@gmail.com>
2021-05-17 13:50:57 +0300

    rework for cli arguments

--- bin/server.dart
+++ bin/server.dart
@@ -1,19 +1,23 @@
-import 'dart:io';
-
+import 'package:args/args.dart';
 import 'package:litgame_server/service/service.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 
-void main() async {
+void main(List<String> arguments) async {
   final service = LitGameRestService();
   await service.init;
 
-  final env = Platform.environment;
-  final host = env['GAME_REST_HOST'];
-  final port = env['GAME_REST_PORT'];
-  if (host == null || port == null) {
-    throw 'No host or port specified';
-  }
+  var host = '';
+  var port = 0;
 
-  final server = await shelf_io.serve(service.handler, host, int.parse(port));
-  print('Server running on localhost:${server.port}');
+  final parser = ArgParser()
+    ..addOption('host', abbr: 'h', defaultsTo: 'localhost')
+    ..addOption('port', abbr: 'p', defaultsTo: 8080.toString());
+
+  final results = parser.parse(arguments);
+
+  host = results['host'];
+  port = int.parse(results['port']);
+
+  final server = await shelf_io.serve(service.handler, host, port);
+  print('Server running on $host:${server.port}');
 }
